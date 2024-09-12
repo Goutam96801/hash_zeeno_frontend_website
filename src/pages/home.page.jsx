876cc2f3e443d9a7946193b300390a719cf9b8f3
Goutam96801 <gautamkumar96801@gmail.com>
2024-09-12 17:58:45 +0530

    added backend deployed url

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 
   // Establish WebSocket connection
   useEffect(() => {
-    const socket = io('http://localhost:5000');  // Ensure the port matches your backend
+    const socket = io('https://hash-zeeno-backend.vercel.app/');  // Ensure the port matches your backend
 
     // Listen for SOS alerts
     socket.on('sosAlert', (data) => {
@@ -27,7 +27,7 @@ const HomePage = () => {
   // Fetch users from the server
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/getAllUsers/');
+      const response = await axios.get('https://hash-zeeno-backend.vercel.app//getAllUsers/');
       setUsers(response.data);
       console.log(response.data);  // Corrected: Log the response data directly
     } catch (err) {
